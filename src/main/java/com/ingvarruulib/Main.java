5316a8babccda029399e81d6531c4fb8cd41d39f
inku0 <ingvar@saarend.com>
2026-01-30 18:44:01 +0200

    usage guidelines

--- src/main/java/com/ingvarruulib/Main.java
+++ src/main/java/com/ingvarruulib/Main.java
@@ -15,7 +15,12 @@ import java.util.EnumSet;
 import static net.dv8tion.jda.api.interactions.commands.OptionType.STRING;
 
 public class Main {
-	static void main(String[] args) {
+	public static void main(String[] args) {
+		if (args.length < 1 || args[0] == null || args[0].isBlank()) {
+			System.err.println("Usage: java -jar <app>.jar <discord-bot-token>");
+			System.exit(2);
+		}
+		
 		JDABuilder.createLight(args[0], EnumSet.of(GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT))
 				.addEventListeners(new MessageReceiverListener())
 				.build();
